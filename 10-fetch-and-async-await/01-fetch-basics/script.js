61bdd11b644cdc75391cb2fe497001b260830fd2
Richard T <richard.tambong04@gmail.com>
2023-05-18 09:37:20 +0800

    .

--- 10-fetch-and-async-await/01-fetch-basics/script.js
+++ 10-fetch-and-async-await/01-fetch-basics/script.js
@@ -25,4 +25,5 @@ fetch('./test.txt')
 fetch('https://api.github.com/users/tropangpotchiph')
   .then((response) => response.json())
   .then((data) => (document.querySelector('h1').textContent = data.login));
+
 //################################
